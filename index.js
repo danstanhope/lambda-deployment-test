d992847a862f6e87ef08564f7861b921b2c66768
Dan Stanhope <danstanhope@gmail.com>
2019-01-07 17:37:31 -0500

    changes

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 exports.handler = (event, context, callback) => {    
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles is: Party time!!!',
+        body: 'The time in Los Angeles is: Party time~~~~',
     });
 };
