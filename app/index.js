076a63ceb786fba0be6dba949eaedb7a8d279206
Dan Stanhope <danstanhope@gmail.com>
2019-01-11 13:12:28 -0500

    changes

--- app/index.js
+++ app/index.js
@@ -1,6 +1,12 @@
 'use strict'
-const awsServerlessExpress = require('aws-serverless-express')
-const app = require('./app')
-const server = awsServerlessExpress.createServer(app)
 
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
+exports.handler = (event, context, callback) => {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    event.Records.forEach((record) => {
+        console.log(record.eventID);
+        console.log(record.eventName);
+        console.log('DynamoDB Record: %j', record.dynamodb);
+    });
+    callback(null, `Successfully processed ${event.Records.length} records.`);
+};
+
